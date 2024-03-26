89aaad358e81817aff757d5e9c62c647f6aa315b
Jammu Kekkonen <jammu.kekkonen@alifsemi.com>
2024-03-26 15:40:09 +0200

    Fix TMC/ETR tracing by disabling SERVICES_set_run_cfg call.   - fix the SERVICES_set_run_cfg parameters as well, but that doesn't help with trace breaking.

--- source/main.c
+++ source/main.c
@@ -8,6 +8,7 @@
 #include "RTE_Components.h"
 #include "audio.h"
 #include "se_services_port.h"
+#include "power.h"
 
 #include CMSIS_device_header
 
@@ -23,7 +24,7 @@ static int init_clocks()
 {
     uint32_t  service_error_code;
     uint32_t  error_code;
-    run_profile_t runp = {0};
+    
 
      /* Initialize the SE services */
     se_services_port_init();
@@ -42,6 +43,8 @@ static int init_clocks()
         printf("SE: MIPI 38.4Mhz(HFOSC) clock enable = %" PRIu32 "\n", error_code);
         return error_code;
     }
+#if 0 // SERVICES_set_run_cfg breaks TCM/ETR so don't use it for now.
+    run_profile_t runp = {0};
 
     /* Get the current run configuration from SE */
     error_code = SERVICES_get_run_cfg(se_services_s_handle,
@@ -55,8 +58,8 @@ static int init_clocks()
 
     // Only MRAM and SRAM0 are used (besides TCMs)
     runp.memory_blocks |= MRAM_MASK | SRAM0_MASK | SRAM1_MASK;
-
     runp.phy_pwr_gating |= MIPI_PLL_DPHY_MASK | MIPI_TX_DPHY_MASK | MIPI_RX_DPHY_MASK | LDO_PHY_MASK;
+    runp.power_domains |= PD_VBAT_AON_MASK | PD_SRAM_CTRL_AON_MASK | PD_SSE700_AON_MASK | PD_RTSS_HE_MASK | PD_SRAMS_MASK | PD_SESS_MASK | PD_SYST_MASK | PD_RTSS_HP_MASK | PD_DBSS_MASK | PD2_APPS_MASK;
 
     /* Set the new run configuration */
     error_code = SERVICES_set_run_cfg(se_services_s_handle,
@@ -67,7 +70,10 @@ static int init_clocks()
         printf("\r\nSE: set_run_cfg error = %" PRIu32 "\n", error_code);
         return error_code;
     }
-
+#else
+    enable_mipi_dphy_power();
+    disable_mipi_dphy_isolation();
+#endif
     return 0;
 }
 
